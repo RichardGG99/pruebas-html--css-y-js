b67ca9e6fb7c3c21ba585d32a36f53b07b63b540
RichardGG99 <135170630+RichardGG99@users.noreply.github.com>
2023-11-21 19:11:17 -0600

    first commit

---
+++ js/indexR.js
@@ -0,0 +1,7 @@
+let toggle = document.getElementById("container");
+let body = document.querySelector("body");
+
+toggle.onclick = function(){
+    toggle.classList.toggle("active");
+    body.classList.toggle("active");
+}
